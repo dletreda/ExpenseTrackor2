caa3bf2dacd23697f724958a90f2a267d63d45a4
dletreda <dlet261221@gmail.com>
2024-06-17 16:35:41 -0400

    first commit

--- src/main/java/com/ExpenseTrackor/ExpenseTrackor/ExpenseTrackorApplication.java
+++ src/main/java/com/ExpenseTrackor/ExpenseTrackor/ExpenseTrackorApplication.java
@@ -2,8 +2,7 @@ package com.ExpenseTrackor.ExpenseTrackor;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
-
+ 
 @SpringBootApplication
  
 public class ExpenseTrackorApplication {
